ba19ecfdbd2d901eb2f0dd05f3634af6310ac0ed
Jonathas Tavares <jonathasl.tavares@gmail.com>
2022-10-31 13:39:37 -0300

    pesquisa e excluir

--- src/main/java/com/algaworks/crm/algacrmapi/controller/ClienteController.java
+++ src/main/java/com/algaworks/crm/algacrmapi/controller/ClienteController.java
@@ -5,6 +5,7 @@ import com.algaworks.crm.algacrmapi.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -18,9 +19,27 @@ public class ClienteController {
     public List<Cliente> listar(){
         return clienteRepository.findAll();
     }
+    @GetMapping("/{id}")
+    public Cliente buscarPorId(@PathVariable Long id) {
+        var clienteOptional = clienteRepository.findById(id);
+        if (clienteOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return clienteOptional.get();
+    }
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente adicionar(@RequestBody Cliente cliente){
         return clienteRepository.save(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void excluirPorId(@PathVariable Long id) {
+        var clienteOptional = clienteRepository.findById(id);
+        if (clienteOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        clienteRepository.delete(clienteOptional.get());
     }
 }
